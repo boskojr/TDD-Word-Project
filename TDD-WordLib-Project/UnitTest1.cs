1b25d76c6e72b4b1630b2fa9ef314263c05e07fb
Bosko Derikonja <derikonja.bosko@gmail.com>
2023-08-29 10:10:07 +0200

    first

---
+++ TDD-WordLib-Project/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿
+using global::WordLib;
+namespace WordLib
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsPalindrome()
+        {
+            // Create an instance to test:
+            Worder worder = new Worder();
+
+            // expected output
+            string expectedResult = "bob";
+            string inputWord = "bob";
+            // Run the method under test:
+            string actualResult = worder.IsPalindromeWordChecker(inputWord);
+
+            // Assert / Verify the result:
+            Assert.AreEqual(expectedResult, actualResult);
+
+              
+        }
+
+        [TestMethod]
+
+        public void IsPalindromeThrowsException()
+        {
+            // Create an instance to test:
+            Worder worder = new Worder();
+
+            // expected output
+            string inputWord = null;
+
+            // Assert / Verify the result:
+            Assert.ThrowsException<ArgumentException>(() => worder.IsPalindromeWordChecker(inputWord));
+
+
+        }
+
+        [TestMethod]
+
+        public void StringLenghtCheck()
+        {
+            // Create an instance to test:
+            Worder worder = new Worder();
+
+            // expected output
+            int expectedOutput = 5;
+            string inputWord = "Paris";
+
+            // Run the method under test:
+            int actualResult = worder.CheckingStringLength(inputWord);
+            // Assert / Verify the result:
+            Assert.AreEqual(expectedOutput, actualResult);
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ TDD-WordLib-Project/WordLib.cs
@@ -0,0 +1,50 @@
+﻿using WordLib;
+
+namespace WordLib
+{
+    public class Worder
+    {
+        public string IsPalindromeWordChecker(string inputWord)
+        {
+
+            // Checking if the empty string by using is null or empty metod
+            if (string.IsNullOrEmpty(inputWord))
+            {
+                throw new ArgumentException("Input string cannot be empty. Please try again");
+            }
+
+            string inputString, reverseString = string.Empty;
+            inputString = inputWord;
+
+
+            /*  Loop through the input word in reverse and build the backwards string
+             * which is "radar" backwards"
+             */
+
+            for (int i = inputWord.Length - 1; i >= 0; i--)
+            {
+                reverseString += inputWord[i];
+            }
+
+            return reverseString;
+
+
+
+        }
+
+        public int CheckingStringLength(string inputWord)
+        {
+
+            // Checking if the empty string by using is null or empty metod
+            if (string.IsNullOrEmpty(inputWord))
+            {
+                throw new ArgumentException("Input string cannot be empty. Please try again");
+            }
+
+            int numbersInLetter = inputWord.Length;
+
+            return numbersInLetter;
+        }
+    }
+}
+
